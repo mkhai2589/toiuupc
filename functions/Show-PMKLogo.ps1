4156e5e55d8664d77f88fcc994b7ab0bd0cdb20d
Minh Khai <57771429+mkhai2589@users.noreply.github.com>
2026-01-31 03:34:11 +0700

    Update Show-PMKLogo.ps1

--- functions/Show-PMKLogo.ps1
+++ functions/Show-PMKLogo.ps1
@@ -3,6 +3,8 @@ function Show-PMKLogo {
     [Console]::OutputEncoding = [System.Text.Encoding]::ASCII
 
     $logo = @(
+
+    
         "PPPPPP  MM    MM  KK   KK   TTTTTT   OOOOO   OOOOO   LL",
         "PP   PP MMM  MMM  KK  KK      TT    OO   OO OO   OO  LL",
         "PPPPPP  MM MM MM  KKKKK       TT    OO   OO OO   OO  LL",
@@ -11,6 +13,9 @@ function Show-PMKLogo {
         "",
         "PMK TOOLBOX - TOI UU WINDOWS",
         "Author: MINH KHAI"
+
+
+        
     )
 
     foreach ($l in $logo) {
